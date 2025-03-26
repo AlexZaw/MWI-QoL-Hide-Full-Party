850b54e0ad4ad5acc4dcc1bca8843278164f290d
Alexandr Zawgorodniy <alexandr.zawgorodniy@gmail.com>
2025-03-27 03:53:13 +0700

    feat: added simple generation of control button

--- js/main.js
+++ js/main.js
@@ -1,44 +1,69 @@
-(new MutationObserver(check)).observe(document, {childList: true, subtree: true});
-
-function check(changes, observer) {
-    if(document.querySelector('.MainPanel_mainPanel__Ex2Ir')) {
-      // actions to perform after selector is found
-        observer.disconnect();
-        init()
-    }
+const hideFullPartyStyle = document.createElement('style');
+hideFullPartyStyle.textContent = `
+.party-btn {
+  position: fixed;
+  right: 0;
+  top: 0;
 }
-function init(){
-
+.hidden-party {
+  display: none;
 }
+`;
+const showHideBtn = document.createElement('button');
+showHideBtn.classList.add('party-btn');
+showHideBtn.innerText = 'Show all';
+document.body.appendChild(showHideBtn);
+showHideBtn.addEventListener('click', showHide);
 
+document.head.appendChild(hideFullPartyStyle);
 
-const newSlot = document.createElement("div");
-newSlot.className = "FindParty_partySlot__NldT8, FindParty_empty__3yQSJ";
 let showFullPartyFlag = false;
 
-showHideBtn.addEventListener("click", showHide);
+new MutationObserver(check).observe(document, {
+    childList: true,
+    subtree: true,
+});
+function check(changes, observer) {
+    if (document.querySelector('.MainPanel_mainPanel__Ex2Ir')) {
+        // actions to perform after selector is found
+        observer.disconnect();
+        init();
+    }
+}
 
 function showHide() {
-  showFullPartyFlag = showFullPartyFlag ? false : true;
-  [...partys].forEach(function (party) {
-    if (getPartySlotsInfo(party).length == 0 && showFullPartyFlag == false) {
-      party.classList.add("hidden-party");
-    } else {
-      party.classList.remove("hidden-party");
-    }
-  });
+    showFullPartyFlag = showFullPartyFlag ? false : true;
+    [...partyList].forEach(function (party) {
+        if (
+            getPartySlotsInfo(party).length == 0 &&
+            showFullPartyFlag == false
+        ) {
+            party.classList.add('hidden-party');
+        } else {
+            party.classList.remove('hidden-party');
+        }
+    });
 
-  if (showFullPartyFlag) {
-    showHideBtn.innerText = "Hide full";
-  } else {
-    showHideBtn.innerText = "Show all";
-  }
+    if (showFullPartyFlag) {
+        showHideBtn.innerText = 'Hide full';
+    } else {
+        showHideBtn.innerText = 'Show all';
+    }
 }
 
 function getPartySlotsInfo(party) {
-  return party
-    .querySelector('[class*="FindParty_partySlots"]')
-    .querySelectorAll('[class*="FindParty_empty"]');
+    return party
+        .querySelector('[class*="FindParty_partySlots"]')
+        .querySelectorAll('[class*="FindParty_empty"]');
 }
 
-showHide();
+//for test only:
+
+// partyListElement = document.querySelector('[class*="FindParty_partyList"');
+// partyList = partyListElement.children;
+
+// const newSlot = document.createElement("div");
+// newSlot.className = "FindParty_partySlot__NldT8, FindParty_empty__3yQSJ";
+
+// showHide();
+check();
