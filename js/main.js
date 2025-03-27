01fcf39357fa6be9c3d880aa65ac9ff3754ab288
Alexandr Zawgorodniy <alexandr.zawgorodniy@gmail.com>
2025-03-28 02:20:57 +0700

    feat: add main functionality

--- js/main.js
+++ js/main.js
@@ -1,43 +1,73 @@
 const hideFullPartyStyle = document.createElement('style');
 hideFullPartyStyle.textContent = `
-.party-btn {
-  position: fixed;
-  right: 0;
-  top: 0;
-}
 .hidden-party {
-  display: none;
+  display: none !important;
 }
 `;
-const showHideBtn = document.createElement('button');
-showHideBtn.classList.add('party-btn');
-showHideBtn.innerText = 'Show all';
-document.body.appendChild(showHideBtn);
-showHideBtn.addEventListener('click', showHide);
 
 document.head.appendChild(hideFullPartyStyle);
+let showHideBtn;
+let showFullPartyFlag = true;
 
-let showFullPartyFlag = false;
-
-new MutationObserver(check).observe(document, {
+new MutationObserver(waitMainPanelElem).observe(document, {
     childList: true,
     subtree: true,
 });
-function check(changes, observer) {
+
+function waitMainPanelElem(changes, observer) {
     if (document.querySelector('.MainPanel_mainPanel__Ex2Ir')) {
-        // actions to perform after selector is found
         observer.disconnect();
-        init();
+        observeMainPanelChanges();
+    }
+}
+
+function observeMainPanelChanges() {
+    console.log('checkMainPanelChanges');
+    new MutationObserver(onMainPanelChange).observe(
+        document.querySelector('.MainPanel_mainPanel__Ex2Ir'),
+        {
+            childList: true,
+            subtree: true,
+        }
+    );
+}
+
+function onMainPanelChange() {
+    try {
+        if (
+            document
+                .querySelector('.FindParty_optionsContainer__3WFfI')
+                .querySelector('.Button_button__1Fe9z')
+        ) {
+            addButton();
+        }
+    } catch (error) {
+        return false;
+    }
+}
+
+function addButton() {
+    if (!document.querySelector('.showHideFullparty-btn')) {
+        showHideBtn = document.createElement('button');
+        showHideBtn.classList.add(
+            'showHideFullparty-btn',
+            'Button_button__1Fe9z'
+        );
+        showHideBtn.innerText = 'Hide full';
+        showHideBtn.addEventListener('click', showHide);
+        document
+            .querySelector('.FindParty_optionsContainer__3WFfI')
+            .appendChild(showHideBtn);
     }
 }
 
 function showHide() {
+    const partyList = document.querySelector(
+        '.FindParty_partyList__3lirO'
+    ).children;
     showFullPartyFlag = showFullPartyFlag ? false : true;
     [...partyList].forEach(function (party) {
-        if (
-            getPartySlotsInfo(party).length == 0 &&
-            showFullPartyFlag == false
-        ) {
+        if (!isFree(party) && showFullPartyFlag == false) {
             party.classList.add('hidden-party');
         } else {
             party.classList.remove('hidden-party');
@@ -51,19 +81,8 @@ function showHide() {
     }
 }
 
-function getPartySlotsInfo(party) {
+function isFree(party) {
     return party
         .querySelector('[class*="FindParty_partySlots"]')
-        .querySelectorAll('[class*="FindParty_empty"]');
+        .querySelectorAll('[class*="FindParty_empty"]').length;
 }
-
-//for test only:
-
-// partyListElement = document.querySelector('[class*="FindParty_partyList"');
-// partyList = partyListElement.children;
-
-// const newSlot = document.createElement("div");
-// newSlot.className = "FindParty_partySlot__NldT8, FindParty_empty__3yQSJ";
-
-// showHide();
-check();
